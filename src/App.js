a8c9289afa6ce57f7f08504022ea20b33edb1120
cnwellman <99782797+cnwellman@users.noreply.github.com>
2022-12-29 23:25:08 -0800

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/chelsea-wellman" target="_blank">
+          Chelsea Wellman
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/cnwellman/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
