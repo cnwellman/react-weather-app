df92dff5990bd26b279da078d34afac65149aee6
cnwellman <99782797+cnwellman@users.noreply.github.com>
2023-01-02 20:39:31 -0800

    Fix link errors

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/chelsea-wellman" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/chelsea-wellman"
+          target="_blank"
+          rel="noreferrer"
+        >
           Chelsea Wellman
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/cnwellman/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub.
         </a>
