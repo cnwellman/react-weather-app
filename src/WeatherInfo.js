e0a2b353f76a8fa50a50fa43a513175d44bd9fd3
cnwellman <99782797+cnwellman@users.noreply.github.com>
2023-01-10 20:42:20 -0800

    Created WeatherTemperature component for temp conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,33 +1,27 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="city">
-        <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
-        </ul>
-      </div>
-      <div className="row">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
         <div className="col-6">
-          <ul>
-            <li className="text-capitalize">{props.data.description}</li>
-          </ul>
           <div className="clearfix d-flex align-items-center">
-            <div className="float-left">
+            <div>
               <WeatherIcon code={props.data.icon} />
             </div>
 
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
