0ebf4187cbc65e3dd4a1ac119766f62584ecb404
cnwellman <99782797+cnwellman@users.noreply.github.com>
2023-01-24 10:39:20 -0800

    Moved forecast into "load" function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,15 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "b400ae3b711a616262d18b0ca2cbe78f";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
+`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -27,19 +36,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
-            return null;
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "b400ae3b711a616262d18b0ca2cbe78f";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
-`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
